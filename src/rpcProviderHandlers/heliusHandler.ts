0ebdba339707f3e524d4212287ac4dede0d0007b
Darwin Rinderer <darwin@helium.foundation>
2023-07-06 15:36:00 -0400

    Tabs be gone

--- src/rpcProviderHandlers/heliusHandler.ts
+++ src/rpcProviderHandlers/heliusHandler.ts
@@ -1,147 +1,147 @@
 import { Env } from '../types';
 
 export const heliusHandler = async ({
-	request,
-	env,
-	heliusErrorHandler,
+  request,
+  env,
+  heliusErrorHandler,
 }: {
-	request: Request;
-	env: Env;
-	heliusErrorHandler: ({
-		env,
-		req,
-		res,
-	}: {
-		env: Env;
-		req: Request;
-		res: Response;
-	}) => Promise<void>;
+  request: Request;
+  env: Env;
+  heliusErrorHandler: ({
+    env,
+    req,
+    res,
+  }: {
+    env: Env;
+    req: Request;
+    res: Response;
+  }) => Promise<void>;
 }) => {
-	// If the request is an OPTIONS request, return a 200 response with permissive CORS headers.
-	// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
-	// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
-	// changing the `*` in the `Access-Control-Allow-Origin` header to a specific origin.
-	// For example, if you wanted to allow requests from `https://example.com`, you would change the
-	// header to `https://example.com`.
-	const origin = request.headers.get('Origin');
-	const corsHeaders: Record<string, string> = {
-		'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
-		'Access-Control-Allow-Headers': '*',
-	};
-	if (env.CORS_ALLOW_ORIGIN) {
-		if (origin && env.CORS_ALLOW_ORIGIN.includes(origin)) {
-			corsHeaders['Access-Control-Allow-Origin'] = origin;
-		}
-	} else {
-		corsHeaders['Access-Control-Allow-Origin'] = '*';
-	}
+  // If the request is an OPTIONS request, return a 200 response with permissive CORS headers.
+  // This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
+  // If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
+  // changing the `*` in the `Access-Control-Allow-Origin` header to a specific origin.
+  // For example, if you wanted to allow requests from `https://example.com`, you would change the
+  // header to `https://example.com`.
+  const origin = request.headers.get('Origin');
+  const corsHeaders: Record<string, string> = {
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
+    'Access-Control-Allow-Headers': '*',
+  };
+  if (env.CORS_ALLOW_ORIGIN) {
+    if (origin && env.CORS_ALLOW_ORIGIN.includes(origin)) {
+      corsHeaders['Access-Control-Allow-Origin'] = origin;
+    }
+  } else {
+    corsHeaders['Access-Control-Allow-Origin'] = '*';
+  }
 
-	// Helius Solana mainnet domains (e.g., mainnet.helius-rpc.com, api.helius.xyz) are the default for all
-	// incoming requests to the CF worker, but if the request originates from solana-rpc.web.test-helium.com,
-	// use the Helius Solana devnet domains (e.g., devnet.helius-rpc.com, api-devnet.helius.xyz)
-	let rpcUrl = 'mainnet.helius-rpc.com';
-	let apiUrl = 'api.helius.xyz';
-	const headers = request.headers;
-	const host = headers.get('Host');
-	if (host == 'solana-rpc.web.test-helium.com') {
-		rpcUrl = 'devnet.helius-rpc.com';
-		apiUrl = 'api-devnet.helius.xyz';
-	}
+  // Helius Solana mainnet domains (e.g., mainnet.helius-rpc.com, api.helius.xyz) are the default for all
+  // incoming requests to the CF worker, but if the request originates from solana-rpc.web.test-helium.com,
+  // use the Helius Solana devnet domains (e.g., devnet.helius-rpc.com, api-devnet.helius.xyz)
+  let rpcUrl = 'mainnet.helius-rpc.com';
+  let apiUrl = 'api.helius.xyz';
+  const headers = request.headers;
+  const host = headers.get('Host');
+  if (host == 'solana-rpc.web.test-helium.com') {
+    rpcUrl = 'devnet.helius-rpc.com';
+    apiUrl = 'api-devnet.helius.xyz';
+  }
 
-	// If the query string session-key value doesn't match the SESSION_KEY env variable, return 404
-	// otherwise continue on, but delete the session-key query string param from the list of all
-	// other query string params
-	const { searchParams, pathname } = new URL(request.url);
-	const sessionKey = searchParams.get('session-key');
-	if (sessionKey != env.SESSION_KEY) {
-		return new Response(null, {
-			status: 404,
-			statusText: 'Unexpected path',
-		});
-	}
-	searchParams.delete('session-key');
+  // If the query string session-key value doesn't match the SESSION_KEY env variable, return 404
+  // otherwise continue on, but delete the session-key query string param from the list of all
+  // other query string params
+  const { searchParams, pathname } = new URL(request.url);
+  const sessionKey = searchParams.get('session-key');
+  if (sessionKey != env.SESSION_KEY) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Unexpected path',
+    });
+  }
+  searchParams.delete('session-key');
 
-	if (request.method === 'OPTIONS') {
-		return new Response(null, {
-			status: 200,
-			headers: corsHeaders,
-		});
-	}
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: corsHeaders,
+    });
+  }
 
-	const upgradeHeader = request.headers.get('Upgrade');
-	if (upgradeHeader || upgradeHeader === 'websocket') {
-		const res = await fetch(`https://${rpcUrl}/?api-key=${env.HELIUS_API_KEY}`, request);
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (upgradeHeader || upgradeHeader === 'websocket') {
+    const res = await fetch(`https://${rpcUrl}/?api-key=${env.HELIUS_API_KEY}`, request);
 
-		// Added because I'm unsure if this path is working after the changes to architecture 
-		console.log(res);
+    // Added because I'm unsure if this path is working after the changes to architecture 
+    console.log(res);
 
-		if (res.status >= 400) {
-			await heliusErrorHandler({ env, res: res.clone(), req: request });
-		}
+    if (res.status >= 400) {
+      await heliusErrorHandler({ env, res: res.clone(), req: request });
+    }
 
-		return res;
-	}
+    return res;
+  }
 
-	const payload = await request.text();
+  const payload = await request.text();
 
-	if (request.method === 'POST') {
-		try {
-			const data = JSON.parse(payload);
+  if (request.method === 'POST') {
+    try {
+      const data = JSON.parse(payload);
 
-			if (data.length === 0) {
-				return new Response(
-					JSON.stringify({
-						jsonrpc: 2.0,
-						id: null,
-						error: { code: -32600, message: 'empty rpc batch' },
-					}),
-					{
-						status: 400,
-						statusText: 'Bad Request',
-					}
-				);
-			}
-		} catch (e) {
-			return new Response(
-				JSON.stringify({
-					jsonrpc: 2.0,
-					id: null,
-					error: { code: -32700, message: 'failed to parse RPC request body' },
-				}),
-				{
-					status: 400,
-					statusText: 'Bad Request',
-				}
-			);
-		}
-	}
+      if (data.length === 0) {
+        return new Response(
+          JSON.stringify({
+            jsonrpc: 2.0,
+            id: null,
+            error: { code: -32600, message: 'empty rpc batch' },
+          }),
+          {
+            status: 400,
+            statusText: 'Bad Request',
+          }
+        );
+      }
+    } catch (e) {
+      return new Response(
+        JSON.stringify({
+          jsonrpc: 2.0,
+          id: null,
+          error: { code: -32700, message: 'failed to parse RPC request body' },
+        }),
+        {
+          status: 400,
+          statusText: 'Bad Request',
+        }
+      );
+    }
+  }
 
-	const proxyHeaders: Record<string, string> = {
-		'Content-Type': 'application/json',
-		'X-Helius-Cloudflare-Proxy': 'true',
-		...corsHeaders,
-	};
+  const proxyHeaders: Record<string, string> = {
+    'Content-Type': 'application/json',
+    'X-Helius-Cloudflare-Proxy': 'true',
+    ...corsHeaders,
+  };
 
-	if (origin) {
-		proxyHeaders['Origin'] = origin;
-	}
+  if (origin) {
+    proxyHeaders['Origin'] = origin;
+  }
 
-	const proxyRequest = new Request(
-		`https://${pathname === '/' ? rpcUrl : apiUrl}${pathname}?api-key=${env.HELIUS_API_KEY}${
-			searchParams.toString() ? `&${searchParams.toString()}` : ''
-		}`,
-		{
-			method: request.method,
-			body: payload || null,
-			headers: proxyHeaders,
-		}
-	);
+  const proxyRequest = new Request(
+    `https://${pathname === '/' ? rpcUrl : apiUrl}${pathname}?api-key=${env.HELIUS_API_KEY}${
+      searchParams.toString() ? `&${searchParams.toString()}` : ''
+    }`,
+    {
+      method: request.method,
+      body: payload || null,
+      headers: proxyHeaders,
+    }
+  );
 
-	const res = await fetch(proxyRequest);
+  const res = await fetch(proxyRequest);
 
-	if (res.status >= 400) {
-		await heliusErrorHandler({ env, res: res.clone(), req: request });
-	}
+  if (res.status >= 400) {
+    await heliusErrorHandler({ env, res: res.clone(), req: request });
+  }
 
-	return res;
+  return res;
 };
